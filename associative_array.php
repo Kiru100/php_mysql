4954a8e179c3a0faa065e10d1148b99a49df9089
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2022-04-21 16:11:19 +0800

    Create associative_array.php

---
+++ associative_array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Associative Array</title>
+</head>
+<body>
+    <?php 
+    $cards = array("Audi" => 50.500,"BMW"=> 40.700, "here"=>60.300);
+
+    print_r($cards);
+    ?>
+</body>
+</html>
