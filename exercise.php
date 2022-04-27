0fb54eca3657f54d4d81d2f97bd5e20c5d06ef93
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2022-04-27 08:13:01 +0800

    Create exercise.php

---
+++ exercise.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php exercise</title>
+</head>
+<body>
+    <?php 
+        $x = 10;
+        $y = 10;
+        
+
+        $z=$x+$y;
+        print $z*5;
+    ?>
+</body>
+</html>
