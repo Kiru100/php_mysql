beaff9df40c4d062b0c62001ecb01a3934521e4a
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2022-04-27 10:51:11 +0800

    Update exercise.php

--- exercise.php
+++ exercise.php
@@ -7,13 +7,35 @@
     <title>php exercise</title>
 </head>
 <body>
+    <h1>exercise number 1</h1>
     <?php 
         $x = 10;
         $y = 10;
-        
-
         $z=$x+$y;
         print $z*5;
     ?>
+
+    <h1>Exercise Number 2</h1>
+    <?php 
+        $price=100;
+        $vat = 0.21;
+        $totalPrice = ($price * $vat)+$price;       
+        echo "Price         : ${price} <br>";
+        echo "Vat: ${vat} <br>";
+        echo "Total Price   : ${totalPrice}";
+    ?>
+
+    <h1>Exercise Number 3</h1>
+    <?php   
+        $x=4;
+        $y=9;
+        $z=4;
+
+        $average=($x + $y + $z)/3;
+        echo "Average : ".number_format($average,2,'.','')
+
+    
+    ?>
+   
 </body>
 </html>
